d6a6c926562c5bd0caa03436566ff11ceb8ac39e
lee-mihye <gnsap@naver.com>
2022-09-08 12:31:03 +0900

    annexcombine

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/// GNB메뉴 링크 연결 JS - linksys.js ///
+
+$(function () { /////// jQB ///////////////////////
+    console.log("메인js");
+
+
+
+    ////////// click //////////////
+
+    $(".click_btn").click(function () {
+        $(this).addClass("active");
+        $(".click_btn .button-text").addClass("active");
+    })
+    $(".main.click_btn").click(function () {
+        $(this).addClass("active");
+        $(".main.click_btn .button-text").addClass("active");
+    })
+
+
+}); ////////// jQB ///////////////////////////////
+/////////////////////////////////////////////////
